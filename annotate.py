0ff5961cfc9396525c84eb449efffa230aaadce7
4g <apurvagup@gmail.com>
2025-10-29 11:35:37 +0530

    generate dataset with trajectories and finetune qwen with and without trajectories

--- annotate.py
+++ annotate.py
@@ -189,11 +189,183 @@ def annotate_videos(videos_dir, annotation_path, output_dir):
     with ThreadPoolExecutor(max_workers=4) as ex:
         results = list(tqdm(ex.map(annotate_video, elems), total=len(elems)))
 
+
+def prepare_labels(annotation_path, labels_dir, out_path):
+    annotations = json.load(open(annotation_path))
+    n_elems = 10000
+    keys = sorted(annotations.keys())[:n_elems]
+    print(keys[:10])
+    keys = set(keys)
+
+    labels_dir = Path(labels_dir)
+    label_files = [labels_dir / 'train.json', labels_dir / 'validation.json']
+    labels = {}
+    for f in label_files:
+        flabels = json.load(open(f))
+        for elem in flabels:
+            name = elem["id"]
+            if name in keys:
+                labels[name] = elem
+    
+
+    with  open(Path(out_path) / 'ssv2_labels.json', 'w') as fp:
+        fp.write(json.dumps(labels))
+
+
+def prepare_training_json(out_path):
+    import re
+
+    def all_between_brackets(s: str) -> list[str]:
+        return re.findall(r'\[(.*?)\]', s)
+
+    labels = json.load(open(Path(out_path) / 'ssv2_labels.json'))
+    data = []
+
+    for name in labels:
+        label = labels[name]
+
+        somethings = all_between_brackets(label["template"].lower())
+        if set(somethings) != {'something'}:
+            continue
+        
+        words = label["template"].lower().replace("something", "").split()
+        for word in words:
+            if word[-3:] == "ing":
+                label["template"] = label["template"].lower().replace(word, '[doing]')
+                label["verb"] = word
+                data.append(label)
+                break
+    
+    objects = set()
+    verbs = set()
+    for label in data:
+        for p in label["placeholders"]:
+            objects.add(p)
+        verbs.add(label["verb"])
+
+    verbs = list(verbs)
+    objects = list(objects)
+
+    n_choices = 4
+
+    test_data = data[:1000]
+    train_data = data[1000:]
+    
+    all_data = {"train": train_data, "test": test_data}
+    all_samples = {}
+
+    for shard in all_data:
+        data = all_data[shard]
+        results = []
+        for label in data:
+            things = label["placeholders"]
+            for i in range(len(things)):
+                label["template"] = label["template"].replace("[something]", f"[something{i}]", 1)
+            
+            replacements = {}
+            for i in range(len(things)):
+                replacements[f"[something{i}]"] = things[i]
+            
+            replacements["[doing]"] = label["verb"]
+            for elem in replacements.items():
+                _ = list(replacements.items())
+                _.remove(elem)
+                tmp = label["template"]
+                for i, j in _:
+                    tmp = tmp.replace(i, j)
+                results.append([elem, tmp])
+        
+        for elem, template in results:
+            if 'something' in elem[0]:
+                type = 'object'
+                cset = objects
+            else:
+                type = 'verb'
+                cset = verbs
+            
+            choices = [elem[1]]
+            while len(choices) < 4:
+                choices.append(random.choice(cset))
+            
+            random.shuffle(choices)
+
+            # print(elem, template, type, choices)
+            
+            all_samples[shard] = all_samples.get(shard, [])
+            sample = {
+                "question": template.replace(elem[0], '_____'),
+                "choices": choices,
+                "answer": elem[1],
+                "video": label["id"]
+            }
+
+            all_samples[shard].append(sample)
+
+    qwen_format = {shard: [] for shard in all_samples}
+    for shard in all_samples:
+        for elem in all_samples[shard]:
+            sample = {
+                "video": f"{elem['video']}.mp4",
+                "conversations": [
+                {
+                    "from": "human",
+                    "value": f"<video>\nLook at the video, and then fill in the blank accordingly from one of the given choices Only output the correct answer. \n" \
+                          f"{elem['question']}. \nChoices: {elem['choices']}"
+                },
+                {
+                    "from": "gpt",
+                    "value": f"{elem['answer']}"
+                },
+                ],
+                "metadata": elem
+            }
+            qwen_format[shard].append(sample)
+    
+    for shard in qwen_format:
+        with  open(Path(out_path) / f'qwen_format_{shard}.json', 'w') as fp:
+            fp.write(json.dumps(qwen_format[shard], indent=True))
+        
+
+def resave_video(elem):
+        video_path, output_video = elem
+        video = VideoDecoder(video_path, dimension_order='NHWC')
+        metadata = video.metadata
+        frames = video.get_frames_in_range(start=0, stop=metadata.num_frames, step=1).data
+        _, H, W, _ = frames.shape
+        frames = frames[:, :(H // 2) * 2,:(W // 2) * 2,:]
+        write_video(str(output_video), frames, 12, 'nhwc')
+
+def prepare_plain_videos(videos_dir, annotation_path, output_dir):
+    annotations = json.load(open(annotation_path))
+    n_elems = 10000
+    keys = sorted(annotations.keys())[:n_elems]
+
+    videos_dir = Path(videos_dir)
+    output_dir = Path(output_dir)
+
+    output_dir.mkdir(exist_ok=True)
+    elems = []
+    for video_name in tqdm(keys):
+        video_path = videos_dir / f'{video_name}.webm'
+        output_video = output_dir / f"{video_name}.mp4"
+        elems.append([video_path, output_video])
+    
+
+    with ThreadPoolExecutor(max_workers=4) as ex:
+        results = list(tqdm(ex.map(resave_video, elems), total=len(elems)))
+    
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--videos', required=True)
-    parser.add_argument('--out', required=True)
-    parser.add_argument('--annotations', required=True)
+    parser.add_argument('--out_trajectory', required=True)
+    parser.add_argument('--out_plain', required=True)
+    parser.add_argument('--bbox', required=True)
+    parser.add_argument('--labels', required=True)
+    
     
     args = parser.parse_args()
-    annotate_videos(args.videos, args.annotations, args.out)
+    # annotate_videos(args.videos, args.bbox, args.out_trajectory)
+    # prepare_plain_videos(args.videos, args.bbox, args.out_plain)
+    # prepare_labels(args.bbox, args.labels, args.out_plain)
+    prepare_training_json(args.out_plain)
---
+++ qwenvl/data/__init__.py
@@ -0,0 +1,45 @@
+import re
+
+SSV2_PLAIN = {
+    "annotation_path": "/home/apurva/projects/video_data/ssv2/videos_plain/qwen_format_train.json",
+    "data_path": "/home/apurva/projects/video_data/ssv2/videos_plain/",
+}
+
+
+SSV2_TRAJECTORY = {
+    "annotation_path": "/home/apurva/projects/video_data/ssv2/videos_plain/qwen_format_train.json",
+    "data_path": "/home/apurva/projects/video_data/ssv2/videos_with_trajectory/",
+}
+
+data_dict = {
+    "ssv2_plain": SSV2_PLAIN, 
+    "ssv2_trajectory": SSV2_TRAJECTORY
+}
+
+
+def parse_sampling_rate(dataset_name):
+    match = re.search(r"%(\d+)$", dataset_name)
+    if match:
+        return int(match.group(1)) / 100.0
+    return 1.0
+
+
+def data_list(dataset_names):
+    config_list = []
+    for dataset_name in dataset_names:
+        sampling_rate = parse_sampling_rate(dataset_name)
+        dataset_name = re.sub(r"%(\d+)$", "", dataset_name)
+        if dataset_name in data_dict.keys():
+            config = data_dict[dataset_name].copy()
+            config["sampling_rate"] = sampling_rate
+            config_list.append(config)
+        else:
+            raise ValueError(f"do not find {dataset_name}")
+    return config_list
+
+
+if __name__ == "__main__":
+    dataset_names = ["cambrian_737k"]
+    configs = data_list(dataset_names)
+    for config in configs:
+        print(config)
